3718b198018ca34d74b460849cb8988347095baf
aizhan010 <aizhanmaratova6@gmail.com>
2024-03-29 18:28:28 -0500

    added terraform files

---
+++ variable.tf
@@ -0,0 +1,27 @@
+variable app_name {
+  type        = string
+  default     = ""
+  description = "the helm chart app name"
+}
+
+variable namespace {
+    type = string
+    default = ""
+    description = "the name in which the resource will be deployed"
+}
+
+variable podlimit {
+    type = number
+    default = 10
+    description = "the mux number of pod on a namespace"
+
+}
+
+variable cpu_max {
+
+}
+
+
+variable mem_max {
+    
+}
